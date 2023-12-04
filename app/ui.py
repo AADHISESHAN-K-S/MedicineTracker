b1cce3719e01808746f834974639f74b94c12587
AADHISESHAN-K-S <aadhiseshankshinekumar@gmail.com>
2023-12-04 18:58:04 +0400

    Updated UI layout

--- app/ui.py
+++ app/ui.py
@@ -19,24 +19,24 @@ class App(Tk):
 		self.grid()
 
 		# configuring columns and rows
-		self.columnconfigure(0, weight=1)
-		self.columnconfigure(1, weight=1)
+		self.columnconfigure(0, weight=2)
+		self.columnconfigure(1, weight=2)
 
-		self.rowconfigure(0, weight=1)
+		self.rowconfigure(0, weight=2)
 		self.rowconfigure(1, weight=1)
 
 		# setting up frames
 		self.medicines_frame = MedicinesFrame(self)
 		self.medicines_frame.initialize()
-		self.medicines_frame.grid(row=0, column=0)
+		self.medicines_frame.grid(row=0, column=0, sticky="nsew", rowspan=2)
 
 		self.details_frame = DetailsFrame(self)
 		self.details_frame.initialize()
-		self.details_frame.grid(row=0, column=1)
+		self.details_frame.grid(row=0, column=1, sticky="nsew")
 
 		self.buttons_frame = ButtonsFrame(self)
 		self.buttons_frame.initialize()
-		self.buttons_frame.grid(row=1, column=1)
+		self.buttons_frame.grid(row=1, column=1, sticky="nsew")
 
 		self.frames = self.winfo_children()
 
@@ -50,31 +50,45 @@ class MedicinesFrame(Frame):
 
 	def initialize(self):
 		self.grid()
-		pass
+		
+		# configuring columns and rows
+		self.columnconfigure(0, weight=1)
+
+		# elements
+		self.heading = Label(self, text="Medicines")
+		self.heading.grid(row=0, column=0, columnspan=2, sticky="ew")
 
 # entry boxes
 class DetailsFrame(Frame):
 	def __init__(self, parent):
-		Frame.__init__(self, parent, bg = "orange", height=TEMP_HEIGHT, width=TEMP_WIDTH)
+		Frame.__init__(self, parent, bg = "orange")
 		print("Added: DetailsFrame")
 
 	def initialize(self):
 		self.grid()
+		
+		# configuring columns and rows
+		self.columnconfigure(0, weight=1)
+		self.columnconfigure(1, weight=1)
 
+		# elements
+		self.heading = Label(self, text="Details")
+		self.heading.grid(row=0, column=0, columnspan=2, sticky="ew")
+		
 		self.sl_no_entry = Entry(self)
-		self.sl_no_entry.grid(row=0, column=0)
+		self.sl_no_entry.grid(row=1, column=0, sticky="ew")
 
 		self.name_entry = Entry(self)
-		self.name_entry.grid(row=0, column=1)
+		self.name_entry.grid(row=1, column=1, sticky="ew")
 
 		self.desc_entry = Entry(self)
-		self.desc_entry.grid(row=1, column=0, rowspan=2)
+		self.desc_entry.grid(row=2, column=0, rowspan=2, sticky="nsew")
 
 		self.duration_entry = Entry(self)
-		self.duration_entry.grid(row=1, column=1)
+		self.duration_entry.grid(row=2, column=1, sticky="ew")
 
 		self.streak_entry = Entry(self)
-		self.streak_entry.grid(row=2, column=1)
+		self.streak_entry.grid(row=3, column=1, sticky="ew")
 
 
 # buttons
@@ -86,17 +100,25 @@ class ButtonsFrame(Frame):
 	def initialize(self):
 		self.grid()
 		
+		# configuring columns and rows
+		self.columnconfigure(0, weight=1)
+		self.columnconfigure(1, weight=1)
+
+		# elements
+		self.heading = Label(self, text="Actions")
+		self.heading.grid(row=0, column=0, sticky="ew", columnspan=2)
+
 		self.add_btn = Button(self, text = "ADD")
-		self.add_btn.grid(row=0, column=0)
+		self.add_btn.grid(row=1, column=0, sticky="ew")
 
 		self.del_btn = Button(self, text = "DELETE")
-		self.del_btn.grid(row=0, column=1)
+		self.del_btn.grid(row=1, column=1, sticky="ew")
 
 		self.take_btn = Button(self, text = "TAKE")
-		self.take_btn.grid(row=1, columnspan=2)
+		self.take_btn.grid(row=2, columnspan=2, sticky="ew")
 
 		self.miss_btn = Button(self, text = "MISS")
-		self.miss_btn.grid(row=2, columnspan=2)
+		self.miss_btn.grid(row=3, columnspan=2, sticky="ew")
 
 
 
