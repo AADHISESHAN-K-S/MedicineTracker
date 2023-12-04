4d273c238de8f5689a58afcd01f21e6f50cc75da
AADHISESHAN-K-S <aadhiseshankshinekumar@gmail.com>
2023-12-04 20:35:01 +0400

    Added Hover effects to MedicinesFrame

--- app/ui.py
+++ app/ui.py
@@ -69,13 +69,19 @@ class MedicinesFrame(Frame):
 		self.columnconfigure(0, weight=1)
 
 		# elements
-		self.heading = Label(self, text="Medicines")
+		self.heading = Label(self, text="Medicines", bg=MedicinesFrame.color_palette[0])
 		self.heading.grid(row=0, column=0, columnspan=2, sticky="ew")
 
 	def new_label(self, id_value, name_value):
-		temp = Label(self, text=name_value)
+		temp = Label(self, text=name_value, bg=MedicinesFrame.color_palette[0])
 		temp.grid(row=self.next_row, column=0, sticky="ew")
 		
+		temp.bind("<Enter>", func=lambda e: temp.config(
+        background=MedicinesFrame.color_palette[2]))
+
+		temp.bind("<Leave>", func=lambda e: temp.config(
+        background=MedicinesFrame.color_palette[0]))
+
 		self.next_row+=1
 
 
@@ -113,7 +119,6 @@ class DetailsFrame(Frame):
 
 		self.streak_entry = Entry(self)
 		self.streak_entry.grid(row=3, column=1, sticky="ew")
-
 
 # buttons
 class ButtonsFrame(Frame):
